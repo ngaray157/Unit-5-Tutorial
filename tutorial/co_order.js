05229e66e86792e3db923a1fdac4abf14360c997
Nicole Garay <ngaray157@west-mec.org>
2020-05-15 19:15:27 -0700

    finished tutorial

--- tutorial/co_order.js
+++ tutorial/co_order.js
@@ -1,3 +1,6 @@
+
+
+
 "use strict";
 
 /*
@@ -28,4 +31,69 @@
    
 */
 
+//Calculate the cost of order
+calcOrder();
 
+window.addEventListener("load", function(){
+      var orderForm = document.forms.orderForm;
+      orderForm.elements.orderDate.value =
+      new Date().toDateString();
+      orderForm.elements.model.focus();
+      //Event handlers for the web form
+      orderForm.elements.model.onchange = calcOrder;
+      orderForm.elements.qty.onchange = calcOrder;
+
+      var planOptions =
+      document.querySelectorAll('input[name = "protection"]');
+      for (var i = 0; i < planOptions.length; i++){
+         planOptions[i].onclick = calcOrder;
+      }
+   }
+);
+
+function calcOrder(){
+   var orderForm = document.forms.orderForm;
+
+   //Calculate the initial cost of the order
+   var mIndex = orderForm.elements.model.selectedIndex;
+   var mCost = orderForm.elements.model.options[mIndex].value;
+   var qIndex = orderForm.elements.qty.selectedIndex;
+   var quantity = orderForm.elements.qty[qIndex].value;
+
+   //Initial cost = cost x quantity
+   var initialCost = mCost * quantity;
+   orderForm.elements.initialCost.value = formatUSCurrency(initialCost);
+
+   //Retrieve the cost of the user's protection plan
+   var pCost = document.querySelector('inputp[name="protection"]:checked').value*quantity;
+   orderForm.elements.protectionCost.value = formatNumber(pCost,2);
+
+   //Calculate the order subtotal
+   orderForm.elements.subtotal.value = formatNumber(initialCost + pCost, 2);
+
+   //Calculate the sales tax
+   var salesTax = 0.05*(initialCost + pCost);
+   orderForm.elements.salesTax.value = formatNumber(salesTax, 2);
+
+   //Calculate the the cost of the total order
+   var totalCost = initialCost + pCost + salesTax;
+   orderForm.elements.totalCost.value = formatUSCurrency(totalCost);
+
+   //Store the order details
+   orderForm.elements.modelName.value =
+   orderForm.elements.model.options[mIndex].text;
+
+   orderForm.elements.protectionName.value = 
+      document.querySelector('input[name="protection"]:checked').nextSibling.nodeValue;
+}
+
+function formatNumber(val, decimals) {
+      return val.toLocaleString(undefined,
+         {minimumFractionDigits : decimals,
+         maximumFractionDigits: decimals});
+}
+
+function formatUSCurrency(val){
+   return val.toLocaleString('en-US',
+   {style: "currency", currency: "USD"});
+}
--- tutorial/co_payment.js
+++ tutorial/co_payment.js
@@ -44,3 +44,131 @@
 
 */
 
+window.addEventListener("load", function(){
+   //Retrieve the field/value pairs from the URL
+   var formData = location.search.slice(1);
+   formData = formData.replace(/\+/g, " ");
+   formData = decodeURIComponent(formData);
+   var formFields = formData.split(/[&=]/g);
+
+   //Write the field values to the order form
+   document.forms.order.elements.orderData.value = formFields[1];
+   document.forms.order.elements.modelName.value = formFields[5];
+   document.forms.order.elements.qty.value = formFields[7];
+   document.forms.order.elements.initialCost.value = 
+   formFields[9];
+   document.forms.order.elements.protectionName.value =
+   formFields[15];
+   document.forms.order.elements.subtotal.value =  formFields[17];
+   document.forms.order.elements.salesTax.value = formFields[19];
+   document.forms.order.elements.totalCost.value = 
+   formFields[21];
+});
+
+window.addEventListener("load", function(){
+   document.getElementById("subButton").onclick = runSubmit;
+   document.getElementById("cardName").oninput = validateName;
+   document.getElementById("cardNumber").oninput = validateNumber;
+   document.getElementById("expMonth").onchange = validateMonth;
+   document.getElementById("expYear").onchange = validateYear;
+   document.getElementById("cvc").oninput = validateCVC;
+});
+
+function validateName(){
+   var cardName = document.getElementById("cardName");
+   if(cardName.validity.valueMissing){
+      cardName.setCustomValidity("Enter your name as it appears on the card");
+   } else{
+      cardName.setCustomValidity("");
+   }
+}
+
+function runSubmit(){
+   validateName();
+   validateCredit();
+   validateMonth();
+   validateYear();
+}
+
+function validateCVC(){
+   var cardCVC = document.getElementById("cvc");
+   var creditCard = document.querySelector('input[name="credit"]:checked').value;
+   if (cardCVC.validtity.valueMissing){
+      cardCVC.setCustomValidity("enter your CVC number");
+   } else if((creditCard === "amex") &&
+   (/^d$/.test(cardCVC.value) === false)) {
+      cardCVC.setCustomValidity("enter a 4-digit CVC number");
+   } else if((creditCard !== "amex") &&
+   (/^d$/.test(cardCVC.value) === false)) {
+      cardCVC.setCustomValidity("enter a 3-digit CVC number");
+   } else{
+      cardCVC.setCustomValidity("");
+   }
+}
+
+function validateMonth(){
+   var cardMonth = document.getElementById("expMonth");
+   if(cardMonth.selectedIndex === 0){
+      cardMonth.setCustomValidity("select the expiration month");
+   } else{
+      cardMonth.setCustomValidity("");
+   }
+}
+
+function validateYear(){
+   var cardYear = document.getElementById("expYear");
+   if(cardYear.selectedIndex === 0){
+      cardYear.setCustomValidity("select the expiration year");
+   } else{
+      cardYear.setCustomValidity("");
+   }
+}
+
+function validateCredit(){
+   var creditCard = document.forms.payment.elements.credit[0];
+   if(creditCard.validtity.valueMissing){
+      creditCard.setCustomValidity("select your credit card");
+   } else{
+      creditCard.setCustomValidity("");
+   }
+}
+
+function validateNumber(){
+   var cardNumber = document.getElementById("cardNumber");
+   if (cardNumber.validity.valueMissing){
+      cardNumber.setCustomValidity("enter your card number");
+   } else if (cardNumber.validity.patternMismatch){
+      cardNumber.setCustomValidty("enter a valid card number");
+   } else if (luhn(cardNumber.value) === false){
+      cardNumber.setCustomValidity("enter a legitimate card number");
+   } else{
+      cardNumber.setCustomValidity("");
+   }
+}
+
+function sumDigits(numStr){
+   var digitTotal = 0;
+   for (var i = 0; i < numStr.length; i++){
+      digitTotal += parseInt(numStr.charAt(i));
+   }
+   return digitTotal;
+}
+
+function luhn(idNum){
+   var string1 = "";
+   var string2 = "";
+   
+   //Retrieve the odd-numbered digits
+   for(var i = idNum.length - 1; i >= 0; i-= 2){
+      string1 += idNum.charAt(i);
+   }
+
+   //Retrieve the even-numbered digits and double them
+   for(var i = idNum.length - 2; i >= 0; i -=2){
+      string2 += 2*idNum.charAt(i);
+   }
+
+   //Return whether the sum of the digits is divisible by 10
+   return.sumDigits(string1 + string2) % 10 === 0;
+}
+
